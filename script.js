1d046bf0da0dfd9b7f4d08241eb21270e5c0b093
Ariella Centlivre <ariellacentlivre@gmail.com>
2015-11-11 22:22:22 -0500

    CSS Changes

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ $(document).ready(function(){
 		var posts= response.data.children;
 	
 		$.each(posts,function(index,post){
-			var $container= $("<div />").appendTo($("body"));
+			var $container= $("<div />").appendTo($(".maincontainer"));
 			var $title= $("<a />").html("<h1>"+ post.data.title +"<h1>").appendTo($container);
 			$title.attr("href", post.data.url);
 			$("<img /> ").attr("src",post.data.thumbnail).appendTo($container);
-			$("<p />").text(post.data.author).appendTo($container);
+			$("<p />").text("User: "+post.data.author).appendTo($container);
 
 			var postDate= new Date(post.data.created*1000);
+			var month= postDate.getUTCMonth()+1;
+			var year= postDate.getUTCFullYear();
+			var day= postDate.getDate();
 
-			$("<p />").text("posted: "+postDate).appendTo($container);
+			$("<p />").text("Posted: "+month+"/"+day+"/"+year).appendTo($container);
+			
 
 			});
 
